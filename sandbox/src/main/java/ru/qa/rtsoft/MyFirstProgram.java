6f3bf76dd6274228557438e6311dbbfd79301ed4
subkorvin <portrait@mail.ru>
2017-02-09 19:03:59 +0300

    Change formatting

--- sandbox/src/main/java/ru/qa/rtsoft/MyFirstProgram.java
+++ sandbox/src/main/java/ru/qa/rtsoft/MyFirstProgram.java
@@ -2,9 +2,9 @@ package ru.qa.rtsoft;
 
 public class MyFirstProgram {
 
-  public static void main (String[] args) {
-    System.out.println ("Hello, World!");
-  
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+
   }
 
 }
