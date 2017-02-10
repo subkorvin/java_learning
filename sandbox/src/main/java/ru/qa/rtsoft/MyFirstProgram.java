fe3f51a585ad1362bcc6804bb2edd2857c861458
subkorvin <portrait@mail.ru>
2017-02-10 16:44:58 +0300

    Back to "Hello, World!"

--- sandbox/src/main/java/ru/qa/rtsoft/MyFirstProgram.java
+++ sandbox/src/main/java/ru/qa/rtsoft/MyFirstProgram.java
@@ -3,12 +3,7 @@ package ru.qa.rtsoft;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    String somebody = "World";
-    System.out.println("Hello, " + somebody + " !");
-
-    int l = 9;
-    int s = l * l;
-    System.out.println("Площадь квадрата со стороной " + l + " равна " + s);
+    System.out.println("Hello, World!");
 
   } 
 
