76389085abb2cf3cab67797b020400ff5bf757f1
subkorvin <portrait@mail.ru>
2017-02-10 17:27:03 +0300

    Several functions has been added

--- sandbox/src/main/java/ru/qa/rtsoft/MyFirstProgram.java
+++ sandbox/src/main/java/ru/qa/rtsoft/MyFirstProgram.java
@@ -3,8 +3,31 @@ package ru.qa.rtsoft;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    hello("World");
+    hello("User");
+    hello("All");
 
-  } 
+    double len = 5;
+    System.out.println("Площадь квадрата со стороной " + len + " равна " + area(len));
+
+    double a = 3;
+    double b = 4;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a, b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double l) {
+    return l * l;
+
+  }
+
+  public static double area (double a, double b) {
+    return a * b;
+
+  }
+
 
 }
