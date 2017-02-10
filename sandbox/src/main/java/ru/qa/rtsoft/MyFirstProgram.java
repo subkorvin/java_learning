16b3f7db825e71e210802e0e8e71008ed34af758
subkorvin <portrait@mail.ru>
2017-02-10 18:08:04 +0300

    Functions to methods

--- sandbox/src/main/java/ru/qa/rtsoft/MyFirstProgram.java
+++ sandbox/src/main/java/ru/qa/rtsoft/MyFirstProgram.java
@@ -8,26 +8,14 @@ public class MyFirstProgram {
     hello("All");
 
     Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + s.l + " равна " + area(s));
+    System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s.area());
 
 
     Rectangle r = new Rectangle(4,7);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + area(r));
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
-
-  public static double area(Square s) {
-    return s.l * s.l;
-
-  }
-
-  public static double area (Rectangle r) {
-    return r.a * r.b;
-
-  }
-
-
 }
