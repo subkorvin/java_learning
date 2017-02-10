d03da6225a9a2c9b0ef6a3b9e955aa1cf8cd1f3a
subkorvin <portrait@mail.ru>
2017-02-10 17:57:51 +0300

    New classes has been added

---
+++ sandbox/src/main/java/ru/qa/rtsoft/Rectangle.java
@@ -0,0 +1,15 @@
+package ru.qa.rtsoft;
+
+/**
+ * Created by korvin on 10.02.2017.
+ */
+public class Rectangle {
+
+  public double a;
+  public double b;
+
+  public Rectangle (double a, double b) {
+    this.a = a;
+    this.b = b;
+  }
+}
---
+++ sandbox/src/main/java/ru/qa/rtsoft/Square.java
@@ -0,0 +1,14 @@
+package ru.qa.rtsoft;
+
+/**
+ * Created by korvin on 10.02.2017.
+ */
+public class Square {
+
+  public double l;
+
+  public Square (double l) {
+    this.l = l;
+
+  }
+}
