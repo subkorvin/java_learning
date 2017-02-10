473a3d57156f6557ea50e1fd53630af967bad0e9
subkorvin <portrait@mail.ru>
2017-02-10 15:45:10 +0300

    Variables and values

--- sandbox/src/main/java/ru/qa/rtsoft/MyFirstProgram.java
+++ sandbox/src/main/java/ru/qa/rtsoft/MyFirstProgram.java
@@ -5,6 +5,10 @@ public class MyFirstProgram {
   public static void main(String[] args) {
     System.out.println("Hello, World!");
 
-  }
+    int l = 9;
+    int s = l * l;
+    System.out.println("Площадь квадрата со стороной " + l + " равна " + s);
+
+  } 
 
 }
