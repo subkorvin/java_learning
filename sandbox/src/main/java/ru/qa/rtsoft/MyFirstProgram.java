89a449df29ceb9f5e39971a6644bbf963c264d04
subkorvin <portrait@mail.ru>
2017-02-12 13:03:28 +0300

    Задание №2

--- sandbox/src/main/java/ru/qa/rtsoft/MyFirstProgram.java
+++ sandbox/src/main/java/ru/qa/rtsoft/MyFirstProgram.java
@@ -1,21 +1,35 @@
 package ru.qa.rtsoft;
 
+
+
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    hello("World");
-    hello("User");
-    hello("All");
 
-    Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s.area());
+    //hello("World");
+    //hello("User");
+    //hello("All");
+
+    //Square s = new Square(5);
+    //System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s.area());
 
 
-    Rectangle r = new Rectangle(4,7);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
+    //Rectangle r = new Rectangle(4,7);
+    //System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
+
+
+    Point p1 = new Point(2, 3);
+    Point p2 = new Point(-1, 5);
+
+    System.out.println("Расстояние между точками равно " + p2.distance(p1));
+
   }
 
-  public static void hello(String somebody) {
-    System.out.println("Hello, " + somebody + "!");
-  }
+  //public static void hello(String somebody) {
+  //  System.out.println("Hello, " + somebody + "!");
+  //}
+
+  /*public static double distance(Point p1, Point p2) {
+  return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+  }*/
 }
