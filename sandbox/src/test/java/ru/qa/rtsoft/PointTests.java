291cc382a4f04cb67dd1a8e916464926287b9d1b
subkorvin <portrait@mail.ru>
2017-02-18 11:16:26 +0300

    Задание №3

--- sandbox/src/test/java/ru/qa/rtsoft/PointTests.java
+++ sandbox/src/test/java/ru/qa/rtsoft/PointTests.java
@@ -9,9 +9,23 @@ import org.testng.annotations.Test;
 public class PointTests {
 
   @Test
-  public void testPoint () {
+  public void testPoint1() {
     Point p1 = new Point(2, 3);
     Point p2 = new Point(-1, 5);
     Assert.assertEquals(p2.distance(p1), 3.605551275463989);
+  }
+
+  @Test
+  public void testPoint2 () {
+    Point p1 = new Point(2, 3);
+    Point p2 = new Point(-1, 5);
+    Assert.assertEquals(p2.distance(p1), 3);
+  }
+
+  @Test
+  public void testPoint3 () {
+    Point p1 = new Point(2, 3);
+    Point p2 = new Point(2, 3);
+    Assert.assertEquals(p2.distance(p1), 3);
   }
 }
