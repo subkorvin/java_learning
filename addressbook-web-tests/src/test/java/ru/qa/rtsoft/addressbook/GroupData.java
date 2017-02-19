8190b5a96a4e454505f5cfba5c0743514a25f896
subkorvin <portrait@mail.ru>
2017-02-19 11:44:55 +0300

    Refactoring

---
+++ addressbook-web-tests/src/test/java/ru/qa/rtsoft/addressbook/GroupData.java
@@ -0,0 +1,25 @@
+package ru.qa.rtsoft.addressbook;
+
+public class GroupData {
+  private final String groupname;
+  private final String groupheader;
+  private final String groupfooter;
+
+  public GroupData(String groupname, String groupheader, String groupfooter) {
+    this.groupname = groupname;
+    this.groupheader = groupheader;
+    this.groupfooter = groupfooter;
+  }
+
+  public String getGroupname() {
+    return groupname;
+  }
+
+  public String getGroupheader() {
+    return groupheader;
+  }
+
+  public String getGroupfooter() {
+    return groupfooter;
+  }
+}
