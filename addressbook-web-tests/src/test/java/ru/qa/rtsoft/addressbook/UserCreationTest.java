e334f566146427446a8a3fc2668823e7afd6e5e7
subkorvin <portrait@mail.ru>
2017-02-19 12:49:47 +0300

    Задание №4

--- addressbook-web-tests/src/test/java/ru/qa/rtsoft/addressbook/UserCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/qa/rtsoft/addressbook/UserCreationTest.java
@@ -39,24 +39,34 @@ public class UserCreationTest {
   private void fillNewUserFormFields(UserData userData) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(userData.getFirst_name());
     wd.findElement(By.name("middlename")).click();
     wd.findElement(By.name("middlename")).clear();
+    wd.findElement(By.name("middlename")).sendKeys(userData.getMiddle_name());
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(userData.getFamily_name());
     wd.findElement(By.name("nickname")).click();
     wd.findElement(By.name("nickname")).clear();
+    wd.findElement(By.name("nickname")).sendKeys(userData.getNickname());
     wd.findElement(By.name("company")).click();
     wd.findElement(By.name("company")).clear();
+    wd.findElement(By.name("company")).sendKeys(userData.getCompany());
     wd.findElement(By.name("address")).click();
     wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(userData.getAddress());
     wd.findElement(By.name("home")).click();
     wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys(userData.getHome_phone());
     wd.findElement(By.name("mobile")).click();
     wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(userData.getCell_phone());
     wd.findElement(By.name("work")).click();
     wd.findElement(By.name("work")).clear();
+    wd.findElement(By.name("work")).sendKeys(userData.getWork_phone());
     wd.findElement(By.name("email")).click();
     wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(userData.getEmail());
   }
 
   private void initNewUserCreation() {
--- addressbook-web-tests/src/test/java/ru/qa/rtsoft/addressbook/UserData.java
+++ addressbook-web-tests/src/test/java/ru/qa/rtsoft/addressbook/UserData.java
@@ -1,26 +1,67 @@
 package ru.qa.rtsoft.addressbook;
 
 public class UserData {
+  private final String first_name;
+  private final String middle_name;
+  private final String family_name;
+  private final String nickname;
+  private final String company;
+  private final String address;
+  private final String home_phone;
+  private final String cell_phone;
+  private final String work_phone;
+  private final String email;
 
+  public UserData(String first_name, String middle_name, String family_name, String nickname, String company, String address, String home_phone, String cell_phone, String work_phone, String email) {
+    this.first_name = first_name;
+    this.middle_name = middle_name;
+    this.family_name = family_name;
+    this.nickname = nickname;
+    this.company = company;
+    this.address = address;
+    this.home_phone = home_phone;
+    this.cell_phone = cell_phone;
+    this.work_phone = work_phone;
+    this.email = email;
   }
 
+  public String getFirst_name() {
+    return first_name;
   }
 
+  public String getMiddle_name() {
+    return middle_name;
   }
 
+  public String getFamily_name() {
+    return family_name;
   }
 
+  public String getNickname() {
+    return nickname;
   }
 
+  public String getCompany() {
+    return company;
   }
 
+  public String getAddress() {
+    return address;
   }
 
+  public String getHome_phone() {
+    return home_phone;
   }
 
+  public String getCell_phone() {
+    return cell_phone;
   }
 
+  public String getWork_phone() {
+    return work_phone;
   }
 
+  public String getEmail() {
+    return email;
   }
 }
