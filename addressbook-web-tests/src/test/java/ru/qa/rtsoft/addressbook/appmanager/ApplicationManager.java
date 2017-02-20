9530f3e013329d49a70df7680fd3d11405c2eaf0
subkorvin <portrait@mail.ru>
2017-02-20 19:05:01 +0300

    AppManager
    Вдогонку к предыдущему коммиту

--- addressbook-web-tests/src/test/java/ru/qa/rtsoft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/qa/rtsoft/addressbook/appmanager/ApplicationManager.java
@@ -3,7 +3,6 @@ package ru.qa.rtsoft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import ru.qa.rtsoft.addressbook.model.UserData;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,6 +13,8 @@ public class ApplicationManager {
 
   FirefoxDriver wd;
 
+  private UserHelper userHelper;
+  private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
 
@@ -32,66 +33,13 @@ public class ApplicationManager {
     wd.get("http://localhost:8080/addressbook/");
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
-    login("admin", "secret");
-  }
-
-  private void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    sessionHelper = new SessionHelper(wd);
+    userHelper = new UserHelper(wd);
+    sessionHelper.login("admin", "secret");
   }
 
   public void stop() {
     wd.quit();
-  }
-
-  public void returnToHomePage() {
-    wd.findElement(By.linkText("home page")).click();
-  }
-
-  public void submitNewUserCreation() {
-    groupHelper.submitGroupCreation();
-  }
-
-  public void fillNewUserFormFields(UserData userData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(userData.getFirst_name());
-    wd.findElement(By.name("middlename")).click();
-    wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys(userData.getMiddle_name());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(userData.getFamily_name());
-    wd.findElement(By.name("nickname")).click();
-    wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys(userData.getNickname());
-    wd.findElement(By.name("company")).click();
-    wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys(userData.getCompany());
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(userData.getAddress());
-    wd.findElement(By.name("home")).click();
-    wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys(userData.getHome_phone());
-    wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(userData.getCell_phone());
-    wd.findElement(By.name("work")).click();
-    wd.findElement(By.name("work")).clear();
-    wd.findElement(By.name("work")).sendKeys(userData.getWork_phone());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(userData.getEmail());
-  }
-
-  public void initNewUserCreation() {
-    wd.findElement(By.linkText("add new")).click();
   }
 
   public GroupHelper getGroupHelper() {
@@ -100,5 +48,13 @@ public class ApplicationManager {
 
   public NavigationHelper getNavigationHelper() {
     return navigationHelper;
+  }
+
+  public SessionHelper getSessionHelper() {
+    return sessionHelper;
+  }
+
+  public UserHelper getUserHelper() {
+    return userHelper;
   }
 }
