ebd34bb83e191149efac8f1b04a15bd3cceffd66
subkorvin <portrait@mail.ru>
2017-03-05 13:12:30 +0300

    Лекция 4.9 (цикл для вычисления максимума заменен на лямбда функцию, создание группы)

--- addressbook-web-tests/src/test/java/ru/qa/rtsoft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/qa/rtsoft/addressbook/tests/GroupCreationTests.java
@@ -13,18 +13,12 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("Test1", "Test2", "Test3");
+    GroupData group = new GroupData("w342xrwrqwerqwerdfd!!!!", "Test2", "Test3");
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (GroupData g :after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
