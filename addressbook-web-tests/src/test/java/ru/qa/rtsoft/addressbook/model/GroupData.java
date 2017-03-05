e72ea33b47a923b41ec89611a156f73340bd623e
subkorvin <portrait@mail.ru>
2017-03-05 12:46:35 +0300

    Лекция 4.8 (нахождение максимального значения в списке, модификация теста создания группы)

--- addressbook-web-tests/src/test/java/ru/qa/rtsoft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/qa/rtsoft/addressbook/model/GroupData.java
@@ -1,19 +1,38 @@
 package ru.qa.rtsoft.addressbook.model;
 
 public class GroupData {
+  private int id;
   private final String groupname;
   private final String groupheader;
   private final String groupfooter;
 
-  public GroupData(String groupname, String groupheader, String groupfooter) {
+  public GroupData(int id, String groupname, String groupheader, String groupfooter) {
+    this.id = id;
     this.groupname = groupname;
     this.groupheader = groupheader;
     this.groupfooter = groupfooter;
   }
 
+  public GroupData(String groupname, String groupheader, String groupfooter) {
+    this.id = 0;
+    this.groupname = groupname;
+    this.groupheader = groupheader;
+    this.groupfooter = groupfooter;
+  }
+
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public int getId() {
+    return id;
+  }
+
   public String getGroupname() {
     return groupname;
   }
+
 
   public String getGroupheader() {
     return groupheader;
@@ -24,24 +43,28 @@ public class GroupData {
   }
 
   @Override
-  public String toString() {
-    return "GroupData{" +
-            "groupname='" + groupname + '\'' +
-            '}';
-  }
-
-  @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
 
     GroupData groupData = (GroupData) o;
 
+    if (id != groupData.id) return false;
     return groupname != null ? groupname.equals(groupData.groupname) : groupData.groupname == null;
   }
 
   @Override
   public int hashCode() {
-    return groupname != null ? groupname.hashCode() : 0;
+    int result = id;
+    result = 31 * result + (groupname != null ? groupname.hashCode() : 0);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "id='" + id + '\'' +
+            ", groupname='" + groupname + '\'' +
+            '}';
   }
 }
--- addressbook-web-tests/src/test/java/ru/qa/rtsoft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/qa/rtsoft/addressbook/tests/GroupModificationTests.java
@@ -4,7 +4,9 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.qa.rtsoft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by korvin on 21.02.2017.
@@ -18,8 +20,13 @@ public class GroupModificationTests extends TestBase {
       app.getGroupHelper().createGroup(new GroupData("Test1", "Test2", "Test3"));
     }
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().modifyGroup(new GroupData("Test2", "Test3", "Test4"), before.size() - 1);
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(), "Test2", "Test3", "Test4");
+    app.getGroupHelper().modifyGroup(group, before.size() - 1);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() - 1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
