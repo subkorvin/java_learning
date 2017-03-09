21951972e316209a889fd6deab56ab2658e0b542
subkorvin <portrait@mail.ru>
2017-03-09 19:04:03 +0300

    Выделена локальная переменная groupNumber

--- addressbook-web-tests/src/test/java/ru/qa/rtsoft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/qa/rtsoft/addressbook/tests/GroupModificationTests.java
@@ -23,12 +23,14 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification () {
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData(before.get(before.size() - 1).getId(), "Test2", "Test3", "Test4");
-    app.getGroupHelper().modifyGroup(group, before.size() - 1); // второй параметр - выбор модифицируемой группы, передается в метод modifyGroup и оттуда - в selectGroup
+    int groupNumber = 1; // выбор номера группы в естественном виде (groupNumber == before.size() - 1)
+    groupNumber = groupNumber - 1; // уменьшаем номер на единицу потому что отсчет идет с нуля
+    GroupData group = new GroupData(before.get(groupNumber).getId(), "Test2", "Test3", "Test4");
+    app.getGroupHelper().modifyGroup(group, groupNumber); // второй параметр - выбор модифицируемой группы, передается в метод modifyGroup и оттуда - в selectGroup
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
 
-    before.remove(before.size() - 1);
+    before.remove(groupNumber);
     before.add(group);
     Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
     before.sort(byId);
