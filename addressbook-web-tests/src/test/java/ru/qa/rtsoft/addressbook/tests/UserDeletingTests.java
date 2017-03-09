4a7b2bc0ee7b394975560f14aa9558f90caa9bc7
subkorvin <portrait@mail.ru>
2017-03-09 17:48:14 +0300

    Исправление теста удаления пользователя

--- addressbook-web-tests/src/test/java/ru/qa/rtsoft/addressbook/tests/UserDeletingTests.java
+++ addressbook-web-tests/src/test/java/ru/qa/rtsoft/addressbook/tests/UserDeletingTests.java
@@ -28,32 +28,33 @@ public class UserDeletingTests extends TestBase {
         System.out.println("Некорректный номер пользователя");
         return;
       }
-    }
-    app.getNavigationHelper().gotoGroupPage();
+    } else {
+      app.getNavigationHelper().gotoGroupPage();
     /*
     * проверка наличия хотя бы одной группы
     * если есть - переходим к созданию пользователя и затем удаляем его
     * если нет - создаем сначала группу, потом создаем пользователя и удаляем его же
     */
-    if (!app.getGroupHelper().isThereAGroup()) { //
-      app.getGroupHelper().createGroup(new GroupData("Test1", "Test2", "Test3"));
-      app.getUserHelper().createUser(new UserData("Vasya", "Yu", "Pupkin", "VasyaPro", "NIICHAVO", "Moscow, Leninsky tupik, 13", "+7 435 1234567", "+7 916 1234567", "+7 495 1234567", "vasya@pupkin.ru", "Test1"));
-      List<UserData> before = app.getUserHelper().getUsersList();
-      app.getUserHelper().deleteUser(before.size() - 1);
+      if (!app.getGroupHelper().isThereAGroup()) { //
+        app.getGroupHelper().createGroup(new GroupData("Test1", "Test2", "Test3"));
+        app.getUserHelper().createUser(new UserData("Vasya", "Yu", "Pupkin", "VasyaPro", "NIICHAVO", "Moscow, Leninsky tupik, 13", "+7 435 1234567", "+7 916 1234567", "+7 495 1234567", "vasya@pupkin.ru", "Test1"));
+        List<UserData> before = app.getUserHelper().getUsersList();
+        app.getUserHelper().deleteUser(before.size() - 1);
 //      app.getNavigationHelper().gotoGroupPage();
 //      app.getGroupHelper().deleteGroup(); // подчистка за собой, удаляем созданную группу
-      List<UserData> after = app.getUserHelper().getUsersList();
-      Assert.assertEquals(after.size(), before.size() - 1); //сравнение размеров списков до и после удаления
-      before.remove(before.size() - 1);
-      Assert.assertEquals(before, after); //сравнение списков целиком
-    } else { // если группа есть, создаем пользователя и удаляем его же
-      app.getUserHelper().createUser(new UserData("Vasya", "Yu", "Pupkin", "VasyaPro", "NIICHAVO", "Moscow, Leninsky tupik, 13", "+7 435 1234567", "+7 916 1234567", "+7 495 1234567", "vasya@pupkin.ru", "Test1"));
-      List<UserData> before = app.getUserHelper().getUsersList();
-      app.getUserHelper().deleteUser(before.size() - 1);
-      List<UserData> after = app.getUserHelper().getUsersList();
-      Assert.assertEquals(after.size(), before.size() - 1); //сравнение размеров списков до и после удаления
-      before.remove(before.size() - 1);
-      Assert.assertEquals(before, after); //сравнение списков целиком
+        List<UserData> after = app.getUserHelper().getUsersList();
+        Assert.assertEquals(after.size(), before.size() - 1); //сравнение размеров списков до и после удаления
+        before.remove(before.size() - 1);
+        Assert.assertEquals(before, after); //сравнение списков целиком
+      } else { // если группа есть, создаем пользователя и удаляем его же
+        app.getUserHelper().createUser(new UserData("Vasya", "Yu", "Pupkin", "VasyaPro", "NIICHAVO", "Moscow, Leninsky tupik, 13", "+7 435 1234567", "+7 916 1234567", "+7 495 1234567", "vasya@pupkin.ru", "Test1"));
+        List<UserData> before = app.getUserHelper().getUsersList();
+        app.getUserHelper().deleteUser(before.size() - 1);
+        List<UserData> after = app.getUserHelper().getUsersList();
+        Assert.assertEquals(after.size(), before.size() - 1); //сравнение размеров списков до и после удаления
+        before.remove(before.size() - 1);
+        Assert.assertEquals(before, after); //сравнение списков целиком
+      }
     }
   }
 }
