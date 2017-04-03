f16641a27f308ce8e0abaf04e0d7d4ce6545a689
subkorvin <portrait@mail.ru>
2017-04-03 14:43:56 +0300

    Добавлен новый модуль для тестов Mantis

---
+++ mantis-tests/src/test/java/ru/qa/rtsoft/mantis/tests/TestBase.java
@@ -0,0 +1,41 @@
+package ru.qa.rtsoft.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.qa.rtsoft.mantis.appmanager.ApplicationManager;
+
+/**
+ * Created by korvin on 20.02.2017.
+ */
+public class TestBase {
+
+
+  protected static ApplicationManager app;
+
+  @BeforeSuite
+  public void setUp() throws Exception {
+    app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+    app.init();
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() {
+    app.stop();
+  }
+
+
+  public static ApplicationManager getApp() {
+    return app;
+  }
+
+//  protected UserData refreshUserData(UserData selectedUser, int userId) {
+//    Users usersAfter = app.db().users();
+//    for (UserData user : usersAfter) {
+//      if (user.getId() == userId) {
+//        selectedUser = user;
+//      }
+//    }
+//    return selectedUser;
+//  }
+}
