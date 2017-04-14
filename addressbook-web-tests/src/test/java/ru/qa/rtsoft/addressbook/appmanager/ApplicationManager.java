44e17c808fcd0843084e0a1876da1287052e32ff
subkorvin <portrait@mail.ru>
2017-04-14 13:56:17 +0300

    реализована возможность удаленного запуска браузера

--- addressbook-web-tests/src/test/java/ru/qa/rtsoft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/qa/rtsoft/addressbook/appmanager/ApplicationManager.java
@@ -1,15 +1,19 @@
 package ru.qa.rtsoft.addressbook.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
@@ -41,12 +45,18 @@ public class ApplicationManager {
 
     dbHelper = new DbHelper();
 
-    if (Objects.equals(browser, BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver();
-    } else if (Objects.equals(browser, BrowserType.CHROME)) {
-      wd = new ChromeDriver();
-    } else if (Objects.equals(browser, BrowserType.IE)) {
-      wd = new InternetExplorerDriver();
+    if ("".equals(properties.getProperty("selenium.server"))) {
+      if (Objects.equals(browser, BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      } else if (Objects.equals(browser, BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      } else if (Objects.equals(browser, BrowserType.IE)) {
+        wd = new InternetExplorerDriver();
+      }
+    } else {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities)
     }
 
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);  //установка тайм-аута для ожидания загрузки страницы
@@ -78,7 +88,7 @@ public class ApplicationManager {
     return userHelper;
   }
 
-  public DbHelper db(){
+  public DbHelper db() {
     return dbHelper;
   }
 }
