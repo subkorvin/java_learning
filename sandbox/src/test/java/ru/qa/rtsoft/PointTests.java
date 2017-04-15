57d937ff77b1b1af06e71a867192588656de73c8
subkorvin <portrait@mail.ru>
2017-04-15 11:01:00 +0300

    Поломал тесты (так надо)

--- sandbox/src/test/java/ru/qa/rtsoft/PointTests.java
+++ sandbox/src/test/java/ru/qa/rtsoft/PointTests.java
@@ -19,13 +19,13 @@ public class PointTests {
   public void testPoint2 () {
     Point p1 = new Point(2, 3);
     Point p2 = new Point(-1, 5);
-    Assert.assertEquals(p2.distance(p1), 3.605551275463989);
+    Assert.assertEquals(p2.distance(p1), 3);
   }
 
   @Test
   public void testPoint3 () {
     Point p1 = new Point(2, 3);
     Point p2 = new Point(2, 3);
-    Assert.assertEquals(p2.distance(p1), 0.0);
+    Assert.assertEquals(p2.distance(p1), 3);
   }
 }
